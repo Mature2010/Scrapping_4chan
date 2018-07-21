5292c6749ac59cc119229d1e04387e50d74a212a
Alfonso Candiani Jaramillo <alfonso.candiani.j@gmail.com>
2018-07-21 14:11:17 -0500

    Update scrapping_4chan.rb

--- scrapping_4chan.rb
+++ scrapping_4chan.rb
@@ -1,3 +1,4 @@
+#!/usr/local/bin/ruby
 require 'nokogiri'
 require 'open-uri'
 
@@ -5,14 +6,12 @@ puts "Insert url: "
 url = gets.chomp
 
 imagenes = []
-i = 0
 
   document = Nokogiri::HTML(open(url))
   div_main = document.css('div.postContainer.replyContainer')
 
   div_main.css('div.file').each do |post|
-    imagenes[i] = 'https:' + post.css('a.fileThumb').attr('href')
-    i += 1
+    imagenes << 'https:' + post.css('a.fileThumb').attr('href')
   end
 
 imagenes.each do |direccion|
